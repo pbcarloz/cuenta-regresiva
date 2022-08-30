554fbeec4be68d1ea6945739482daa411dc0da23
pbcarloz <pb_carlos@outlook.com>
2022-08-30 15:27:29 -0500

    updated the page

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const countDown = () => {
     const countDate = new Date ('November 01, 2022 00:00:00').getTime();
+    const countKoseki = new Date ('October 05, 2022 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
+    const gapKoseki = countKoseki - now;
 
     const second = 1000;
     const minute = second * 60;
@@ -17,6 +19,9 @@ const countDown = () => {
     document.querySelector('.hour').innerText = textHour;
     document.querySelector('.minute').innerText = textMinute;
     document.querySelector('.second').innerText = textSecond;
+
+    const kosekiDay = Math.floor(gapKoseki / day);
+    document.querySelector('.koseki').innerText = kosekiDay;
 
     if (gap < 1000) {
         document.querySelector('.tittle').innerText = '<3';
