5748164531781c2c6d3cb6e97a78e590d31a0cd9
pbcarloz <pb_carlos@outlook.com>
2023-01-22 17:31:48 -0600

    Fixed date

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const countDown = () => {
-    const countDate = new Date ('November 26, 2022 00:00:00').getTime();
-    const countKoseki = new Date ('November 26, 2022 00:00:00').getTime();
+    const countDate = new Date ('February 8, 2023 00:00:00').getTime();
+    const countKoseki = new Date ('February 8, 2023 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
     const gapKoseki = countKoseki - now;
